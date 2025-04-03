0516d4c4524004aa056da64e3f5f1500ac2a7a78
Your Name <you@example.com>
2025-04-03 14:17:53 +0000

    du bon poisson

--- projet/main.py
+++ projet/main.py
@@ -2,36 +2,27 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from irobot_create_msgs.msg import HazardDetectionVector, IrIntensityVector  # Import IrIntensityVector
+from rclpy.qos import qos_profile_sensor_data
 
 class ICreate3Basic(Node):
     def __init__(self):
         super().__init__('icreate3_basic')
         
-        # Publisher pour envoyer des commandes de mouvement
         self.cmd_vel_publisher = self.create_publisher(Twist, '/Robot4/cmd_vel', 10)
         
-        # Subscriber pour écouter les capteurs (ex: détection d'obstacles)
         self.hazard_subscriber = self.create_subscription(
             HazardDetectionVector,
             '/Robot4/hazard_detection',
             self.hazard_callback,
-            10)
+            qos_profile_sensor_data)
         
-        # Subscriber pour écouter les données d'intensité IR
-        self.ir_intensity_subscriber = self.create_subscription(
-            IrIntensityVector,
-            '/Robot4/ir_intensity',
-            self.ir_intensity_callback,
-            10)
-        
-        # Timer pour envoyer des commandes de mouvement toutes les 2 secondes
-        self.timer = self.create_timer(2.0, self.move_forward)
+        self.timer = self.create_timer(0.4, self.move_forward)
         self.get_logger().info("Nœud iCreate3 démarré.")
 
     def move_forward(self):
         """ Envoie une commande pour avancer tout droit """
         twist = Twist()
-        twist.linear.x = 0.2  # Avancer à 0.2 m/s
+        twist.linear.x = 0.4  # Avancer à 0.2 m/s
         twist.angular.z = 0.0  # Pas de rotation
         self.cmd_vel_publisher.publish(twist)
         self.get_logger().info("Commande envoyée : avancer")
@@ -42,17 +33,11 @@ class ICreate3Basic(Node):
             self.get_logger().info(f"Détection de danger : {msg.detections}")
             self.get_logger().warn("Obstacle détecté ! Arrêt du robot.")
             twist = Twist()  # Stopper le robot
+            twist.linear.x = -0.5
             self.cmd_vel_publisher.publish(twist)
-
-    def ir_intensity_callback(self, msg):
-        """ Callback pour les données d'intensité IR """
-        for reading in msg.readings:
-            self.get_logger().info(f"Intensité IR : {reading.value}")
-            if reading.value > 1000:  
-                self.get_logger().warn(f"Obstacle détecté par IR ! Intensité : {reading.value}")
-                twist = Twist() 
-                self.cmd_vel_publisher.publish(twist)
-                return  
+            twist = Twist()
+            twist.angular.z = 1.8
+            self.cmd_vel_publisher.publish(twist)
 
 def main(args=None):
     rclpy.init(args=args)
