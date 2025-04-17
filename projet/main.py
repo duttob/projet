d46e77a0f39dcb877a85e9ae29ae5854142d02d2
hugorouillard <rouillard.hugo1@gmail.com>
2025-04-17 15:49:05 +0200

    update ir sensor threshold

--- projet/main.py
+++ projet/main.py
@@ -184,8 +184,7 @@ class RobotControlNode(Node):
 
     def ir_callback(self, msg):
         for reading in msg.readings:
-            self.get_logger().info(str(reading.value))
-            if reading.value > 10:
+            if reading.value > 40:
                 self.get_logger().warn("IR intense: stop")
                 self.stop_robot()
                 self.blocked = True
